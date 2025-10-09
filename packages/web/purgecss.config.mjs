7f5dd66b84341dbbf6a818a250b9afcf787dee33
tooppoo <10760022+tooppoo@users.noreply.github.com>
2025-10-09 18:36:51 +0000

    Auto commit with fmt

--- packages/web/purgecss.config.mjs
+++ packages/web/purgecss.config.mjs
@@ -1,18 +1,12 @@
-
 /**
  * https://github.com/FullHuman/purgecss/issues/1327
  * ドキュメント通りにcommonjsで設定ファイルを書くと動作しない
  * ESMとして定義し、さらに各オプションを個別にexport
  */
 
-export const content = [
-  'dist/**/*.html',
-  'dist/**/*.js',
-  'src/**/*.svelte',
-]
+export const content = ['dist/**/*.html', 'dist/**/*.js', 'src/**/*.svelte']
 export const css = ['dist/_app/immutable/assets/*.css']
 export const output = 'dist/_app/immutable/assets/'
 export const variables = true
 export const fontFace = true
 export const keyframes = true
-
