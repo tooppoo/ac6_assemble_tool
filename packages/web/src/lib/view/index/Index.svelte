bb86eb379fd2ae03b05aa42ff3a3cb1fc98aa53d
philomagi <tooppoo56@gmail.com>
2025-10-27 07:13:18 +0000

    フッターを共通レイアウトとして切り出し

--- packages/web/src/lib/view/index/Index.svelte
+++ packages/web/src/lib/view/index/Index.svelte
@@ -5,7 +5,6 @@
   import Margin from '$lib/components/spacing/Margin.svelte'
   import i18n from '$lib/i18n/define'
   import { useWithEnableState } from '$lib/ssg/safety-reference'
-  import { appVersion } from '$lib/utils/app-version'
 
   import {
     type Assembly,
@@ -438,45 +437,6 @@
     </a>
   </ToolSection>
 </article>
-
-<hr class="my-3" />
-
-<footer class="text-center mb-3">
-  <div>
-    Created by
-    <a
-      id="link-to-linktr"
-      href="https://linktr.ee/Philomagi"
-      rel="external noopener noreferrer"
-    >
-      >Philomagi</a
-    >
-  </div>
-  <div>
-    Source code is managed at
-    <a
-      id="link-to-src"
-      href="https://github.com/tooppoo/ac6_assemble_tool/"
-      rel="external noopener noreferrer">Github</a
-    >
-  </div>
-  <div>
-    App Version v{appVersion}
-  </div>
-  <div class="my-3">
-    <a
-      href="https://www.buymeacoffee.com/philomagi"
-      aria-label="donate to philomagi via 'Buy Me a Coffee'"
-      target="_blank"
-      rel="external noopener noreferrer"
-    >
-      <img
-        src="https://img.buymeacoffee.com/button-api/?text=Donate&emoji=☕&slug=philomagi&button_colour=5F7FFF&font_colour=ffffff&font_family=Comic&outline_colour=000000&coffee_colour=FFDD00"
-        alt="coffee icon"
-      />
-    </a>
-  </div>
-</footer>
 
 <RandomAssemblyOffCanvas
   id="random-assembly-canvas"
