6c8768431a74cda00efe09afabefd2224e60063c
philomagi <tooppoo56@gmail.com>
2025-11-06 09:33:23 +0000

    add EN i18n

--- packages/web/src/lib/i18n/locales/en/assembly.ts
+++ packages/web/src/lib/i18n/locales/en/assembly.ts
@@ -12,32 +12,133 @@ export const enAssembly = {
   generator: 'GENERATOR',
   expansion: 'EXPANSION',
 
+  // arm units
+  attackType: 'ATTACK TYPE',
+  weaponType: 'WEAPON TYPE',
+  attackPower: 'ATTACK POWER',
+  impact: 'IMPACT',
+  accumulativeImpact: 'ACCUMULATIVE IMPACT',
+
+  chargeAttackPower: 'CHARGE ATTACK POWER',
+  chargeImpact: 'CHARGE IMPACT',
+  chargeAccumulativeImpact: 'CHARGE ACCUMULATIVE IMPACT',
+
+  directHitAdjustment: 'DIRECT HIT ADJUSTMENT',
+  recoil: 'RECOIL',
+  idealRange: 'IDEAL RANGE',
+  effectiveRange: 'EFFECTIVE RANGE',
+  rapidFire: 'RAPID FIRE',
+  chargeTime: 'CHARGE TIME',
+  magazineRounds: 'MAGAZINE ROUNDS',
+  totalRounds: 'TOTAL ROUNDS',
+  reloadTime: 'RELOAD TIME',
+  ammunitionCost: 'AMMUNITION COST',
+
+  chargeHeatBuildup: 'CHARGE HEAT BUILDUP',
+  cooling: 'COOLING',
+  heatBuildup: 'HEAT BUILDUP',
+  blastRadius: 'BLAST RADIUS',
+
+  chargeEnLoad: 'CHARGE EN LOAD',
+  chargeAmmoConsumption: 'CHARGE AMMO CONSUMPTION',
+  chargeBlastRadius: 'CHARGE BLAST RADIUS',
+  paInterference: 'PA INTERFERENCE',
+
+  guidance: 'GUIDANCE',
+  homingLockTime: 'HOMING LOCK TIME',
+  lockCount: 'LOCK COUNT',
+
+  consecutiveHits: 'CONSECUTIVE HITS',
+
+  // back units
+  damageMitigation: 'DAMAGE MITIGATION',
+  impactDampening: 'IMPACT DAMPENING',
+  igDamageMitigation: 'IG DAMAGE MITIGATION',
+  igImpactDampening: 'IG IMPACT DAMPENING',
+  igDuration: 'IG DURATION',
+  deployHeatBuildup: 'DEPLOY HEAT BUILDUP',
+  deployment: 'DEPLOYMENT',
+  idleDamageMitigation: 'IDLE DAMAGE MITIGATION',
+  idleImpactDampening: 'IDLE IMPACT DAMPENING',
+  idleDuration: 'IDLE DURATION',
+
+  // misc stats
   ap: 'AP',
   antiKineticDefense: 'KINETIC DEFENSE',
   antiEnergyDefense: 'EN DEFENSE',
   antiExplosiveDefense: 'EXPLOSIVE DEFENSE',
   attitudeStability: 'STABILITY',
 
+  // head
+  systemRecovery: 'SYSTEM RECOVERY',
+  scanDistance: 'SCAN DISTANCE',
+  scanEffectDuration: 'SCAN EFFECT DURATION',
+  scanStandbyTime: 'SCAN STANDBY TIME',
+
+  // core
+  boosterEfficiencyAdjective: 'BOOSTER EFFICIENCY ADJECTIVE',
+  generatorOutputAdjective: 'GENERATOR OUTPUT ADJECTIVE',
+  generatorSupplyAdjective: 'GENERATOR SUPPLY ADJECTIVE',
+
+  // arms
+  armsLoadLimit: 'ARMS LOAD LIMIT',
+  recoilControl: 'RECOIL CONTROL',
+  firearmSpecialization: 'FIREARM SPECIALIZATION',
+  melesSpecialization: 'MELES SPECIALIZATION',
+
+  // legs
+  loadLimit: 'LOAD LIMIT',
+  jumpDistance: 'JUMP DISTANCE',
+  jumpHeight: 'JUMP HEIGHT',
+  travelSpeed: 'TRAVEL SPEED',
+  highSpeedPerformance: 'HIGH SPEED PERFORMANCE',
+
+  // fcs
+  closeRangeAssist: 'CLOSE RANGE ASSIST',
+  mediumRangeAssist: 'MEDIUM RANGE ASSIST',
+  longRangeAssist: 'LONG RANGE ASSIST',
+  missileLockCorrection: 'MISSILE LOCK CORRECTION',
+  multiLockCorrection: 'MULTI LOCK CORRECTION',
+
+  // booster
+  thrust: 'THRUST',
+  upwardThrust: 'UPWARD THRUST',
+  upwardEnConsumption: 'UPWARD EN CONSUMPTION',
+  qbThrust: 'QB THRUST',
+  qbJetDuration: 'QB JET DURATION',
+  qbEnConsumption: 'QB EN CONSUMPTION',
+  qbReloadTime: 'QB RELOAD TIME',
+  qbReloadIdealWeight: 'QB RELOAD IDEAL WEIGHT',
+  abThrust: 'AB THRUST',
+  abEnConsumption: 'AB EN CONSUMPTION',
+  meleeAttackThrust: 'MELEE ATTACK THRUST',
+  meleeAttackEnConsumption: 'MELEE ATTACK EN CONSUMPTION',
+
+  // generator
+  enCapacity: 'EN CAPACITY',
+  enRecharge: 'EN RECHARGE',
+  supplyRecovery: 'SUPPLY RECOVERY',
+  postRecoveryEnSupply: 'POST RECOVERY EN SUPPLY',
+  energyFirearmSpec: 'EN FIREARM SPEC',
+  enOutput: 'EN OUTPUT',
+
+  // expansion
+  durability: 'DURABILITY',
+  timeLimit: 'TIME LIMIT',
+
+  // total status
   weight: 'WEIGHT',
   load: 'LOAD',
-  loadLimit: 'LOAD LIMIT',
   armsLoad: 'ARMS LOAD',
-  armsLoadLimit: 'ARMS LOAD LIMIT',
-  melesSpecialization: ' MELES SPECIALIZATION',
-  melesRatio: ' MELES RATIO',
+  melesRatio: 'MELES RATIO',
 
-  enLoad: 'EN LOAD',
-  enOutput: 'EN OUTPUT',
-  enCapacity: 'EN CAPACITY',
-  enSurplus: 'EN SURPLUS',
-  enSupplyEfficiency: 'EN SUPPLY',
-  enRechargeDelay: 'EN RECHARGE',
-  enRecoveryDelay: 'EN RECOVERY',
-  postRecoveryEnSupply: 'RECOVERY EN SUPPLY',
-  enFirearmSpec: 'EN FIREARM SPECIALIZATION',
+  enFirearmSpec: 'EN FIREARM SPEC',
   enFirearmRatio: 'EN FIREARM RATIO',
-
-  qbEnConsumption: 'QB EN CONSUMPTION',
+  enRechargeDelay: 'EN RECHARGE DELAY',
+  enRecoveryDelay: 'EN RECOVERY DELAY',
+  enLoad: 'EN LOAD',
+  enSurplus: 'EN SURPLUS',
+  enSupplyEfficiency: 'EN SUPPLY EFFICIENCY',
 
   price: 'PRICE',
 
