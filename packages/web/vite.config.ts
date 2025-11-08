bbf8e4dd8819dda3e623603dcebdc722ecb5bb9d
philomagi <tooppoo56@gmail.com>
2025-11-08 15:15:01 +0000

    change setup test

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,11 +2,8 @@ import { execSync } from 'child_process'
 import fs from 'fs'
 
 import { sveltekit } from '@sveltejs/kit/vite'
-import { svelteTesting } from '@testing-library/svelte/vite'
 import { analyzer } from 'vite-bundle-analyzer'
 import { defineConfig } from 'vitest/config'
-
-const isVitest = !!process.env.VITEST
 
 const pkg = (() => {
   const raw = fs.readFileSync('package.json', { encoding: 'utf-8' })
@@ -42,7 +39,6 @@ export default defineConfig({
   },
   plugins: [
     sveltekit(),
-    svelteTesting(),
     (() => {
       console.log(`ANALYZE_MODE=${process.env.ANALYZE_MODE}`)
       switch (process.env.ANALYZE_MODE) {
@@ -66,30 +62,6 @@ export default defineConfig({
   resolve: {
     alias: {
       $lib: '/src/lib',
-      ...(isVitest
-        ? {
-            '$env/static/public': '/src/test/$env-static-public.ts',
-            'svelte-i18next': '/src/test/svelte-i18next-stub.ts',
-          }
-        : {}),
     },
-  },
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-    environment: 'jsdom',
-    testTimeout: 10 * 1000,
-    coverage: {
-      reporter: ['text', 'json'],
-      exclude: [
-        '*rc.cjs',
-        '.svelte-kit/',
-        '*.config.*',
-        '**/**/*.d.ts',
-        'dist/**/*',
-        'vitest-setup.ts',
-      ],
-      provider: 'v8',
-    },
-    setupFiles: ['./vitest-setup'],
   },
 })
