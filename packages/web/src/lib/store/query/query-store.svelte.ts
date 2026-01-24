11c23ce0c411bceb4f0fdecda7afeec64f1aaf0b
philomagi <tooppoo56@gmail.com>
2026-01-24 20:06:42 +0900

    fix

--- packages/web/src/lib/store/query/query-store.svelte.ts
+++ packages/web/src/lib/store/query/query-store.svelte.ts
@@ -6,6 +6,7 @@ import { mergeAssemblyParams } from './query-merge'
 import { browser } from '$app/environment'
 import { pushState } from '$app/navigation'
 import { page } from '$app/state'
+import { SvelteURLSearchParams } from 'svelte/reactivity'
 
 let query: string = $state('')
 export function getQuery(): string {
@@ -24,8 +25,8 @@ export function buildQueryFromAssembly(assembly: Assembly): URLSearchParams {
   const assemblyParams = assemblyToSearchV2(assembly)
   // SSR時はpage.urlにアクセスできないため、空のパラメータを使用
   const mergedParams = browser
-    ? new URLSearchParams(page.url.searchParams)
-    : new URLSearchParams()
+    ? new SvelteURLSearchParams(page.url.searchParams)
+    : new SvelteURLSearchParams()
   mergeAssemblyParams(mergedParams, assemblyParams)
 
   return mergedParams
